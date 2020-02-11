79520b7e67c0fbbd271e590d1166d647afa859ae
Torrent Glenn <toglenn@microsoft.com>
2020-02-10 17:33:33 -0800

    real psm1

---
+++ FruitBat/FruitBat.psm1
@@ -0,0 +1,10 @@
+Import-Module $PSScriptRoot\FruitBat.dll
+
+function Pretty-Commits
+{ 
+    [CmdletBinding()]
+    param([parameter(ValueFromPipeLine = $true)][string[]]$commits)
+    $commits | %{ [pscustomobject]@{ commit = ($_); author = (git show -s --format="%an" $_); message = (git show -s --format="%s" $_); link = "https://msazure.visualstudio.com/One/_git/EngSys-Acis-Legacy/commit/$_" } }
+ }
+
+Export-ModuleMember -Function * -Alias * 
