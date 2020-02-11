995fd7ad6e2ad5350bc7a28ecac6f7d7caec7b77
Torrent Glenn <toglenn@microsoft.com>
2020-02-10 16:53:54 -0800

    add simple module file

--- scripts/Utility.ps1
+++
@@ -1,6 +0,0 @@
-function Pretty-Commits
-{ 
-    [CmdletBinding()]
-    param([parameter(ValueFromPipeLine = $true)][string[]]$commits)
-    $commits | %{ [pscustomobject]@{ commit = ($_); author = (git show -s --format="%an" $_); message = (git show -s --format="%s" $_); link = "https://msazure.visualstudio.com/One/_git/EngSys-Acis-Legacy/commit/$_" } }
- }
